b42de105c996636233c3a820af3630921cd3cb91
aaron-lethers <69817429+aaron-lethers@users.noreply.github.com>
2022-06-03 18:08:13 -0700

    new app.py
    started fresh. connecting to spotify api directly, no libs

--- app.py
+++ app.py
@@ -1,66 +1,85 @@
 from flask import Flask, request, render_template, redirect, flash, session, url_for
-import spotipy 
-from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
-import time
+import base64
+import json
+import requests
 
 app = Flask(__name__)
 
+"""
+ENVIORNMENT VARS:
 # found on spotify dashboard
+
+scope = "user-library-read"							# what information i can get from spotify
+state = 'je73hcyejwkdjen6'							# random 16 char str (i just typed 16 random letters)
+url = 'https://accounts.spotify.com/authorize?'
+"""
+state = 'je73hcyejwkdjen6'
+redirect_uri = 'http://127.0.0.1:5000/redirect_page'
 client_id = '64323a9307734d05b0a1a3176db9a688'		# unique id for our app
 client_secret = '9c507901fae7407dbacd4a9409d19ab8' 	# this is our super secret key
-scope = "user-library-read"							# what information i can get from spotify
-# redirect_uri										# where does spotify redirect the user after oauth
 
-# config
-app.secret_key='secret_key'
+"""
+Request authorization from user to access data
+Requires: response_type, client_id, scope, redirect_uri, state
 
-def create_oauth():
-	return SpotifyOAuth(
-		scope=scope, 
-		client_id=client_id, 
-		client_secret=client_secret, 
-		redirect_uri=url_for('redirect_page', _external=True))
-
-def get_token():
-	token_info = session['token']			# remember we saved this token as session['token'] (its under the redirect_page func)
-	if not token_info:						# token_info can expire, we will need a new one
-		raise "exception"
-	now = int(time.time())
-
-	is_expired = token_info['expires_at'] - now
-	if is_expired < 60:
-		sp = create_oauth()
-		token_info = sp.refresh_access_token(token_info['refresh_token'])
-	return token_info
-
-# every time we launch this app we will need to get oauth from spotify, and this will redirect us to "redirect_page"
+"""
 @app.route('/')
-def oauth():
-	sp = create_oauth()						# creating a new spotify oauth
-	auth_url = sp.get_authorize_url()		# redirect url
-	return redirect(auth_url)
+def req_auth():
+	url = 'https://accounts.spotify.com/authorize?'
 
-# where spotify will redirect our user after oauth
-@app.route('/redirect_page')			
+	auth_info = [
+		'response_type=code',
+		'client_id=64323a9307734d05b0a1a3176db9a688',
+		'scope=user-library-read',
+		'redirect_uri=' + redirect_uri,
+		'state=' + state
+	]
+
+	query = url + '&'.join(auth_info)
+	# print("QUERY: ", query)
+	resp = requests.get(url=query)
+	print('RESP: ', resp)
+
+	return redirect(query)
+
+"""
+Request access and refresh tokens
+Requires: client_id, client_secret, grant_type, code, redirect_uri
+
+"""
+@app.route('/redirect_page')
 def redirect_page():
-	sp = create_oauth()
-	session.clear()							# added some notes on session in the README
-	code = request.args.get('code')			# getting special "code" from spotify oauth
-	token_info = sp.get_access_token(code)	# some spotipy func
-	session['token'] = token_info			# saving token info in session
-	return redirect(url_for('get_tracks'))
+	code = request.args.get('code')
+	print('CODE: ', code)
+	res_state = request.args.get('state')
+	print('STATE: ', state)
 
-@app.route('/get_tracks')
-def get_tracks():
-	try:
-		token_info = get_token()
-	except:
-		print("user not logged in")
-		return redirect(url_for('oauth'))
-	sp = spotipy.Spotify(auth=token_info['access_token'])
-	data = sp.current_user_saved_tracks(limit=50, offset=0)
+	if res_state != state:
+		# flash message
+		return redirect(url_for('req_auth'))
+	else:
+		if not code:
+			error = request.args.get('error')
+			print("User did not accept access: ", error)
+			# implement flash message
+			return redirect(url_for('req_auth'))
 
-	return render_template('get_tracks.html', data=data)
+	userpass = client_id + ':' + client_secret
+	encoded = 'Basic ' + base64.b64encode(userpass.encode('utf-8')).decode('utf-8')
+	url = 'https://accounts.spotify.com/api/token'
+	data = {
+		'code': code,
+		'redirect_uri': redirect_uri,
+		'grant_type': 'authorization_code'
+	}
+	headers = {
+		'Authorization': encoded
+	}
+
+	resp = requests.post(url=url, data=data, headers=headers).json()
+	# print(resp)
+
+	return render_template('redirect_page.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
